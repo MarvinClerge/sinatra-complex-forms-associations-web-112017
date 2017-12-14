c502c2638003d177f33b3dd7a11e4c21ca468bf7
Marvin Clerge <clergemarvin@gmail.com>
2017-12-13 22:09:24 -0500

    Added new and edit for owners and pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,46 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    # binding.pry
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.find_or_create_by(params["owner"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    @owner = @pet.owner
+    erb :"/pets/edit"
+  end
+
+  post '/pets/:id' do
+    @owner = Owner.find_by(params["owner"])
+    @pet = Pet.find(params[:id])
+    @pet.name = params["pet"]["name"]
+    @pet.owner = @owner
+
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.find_or_create_by(name: params["owner_name"])
+    end
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
